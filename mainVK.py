1cad1e4960949afb8fc61380e21fb6980a35e844
Leovair <Leovair@users.noreply.github.com>
2018-12-08 18:32:47 +0200

    Базовый файл в котором будет использоваться программа

--- mainVK.py
+++ mainVK.py
@@ -1,11 +1,11 @@
+import requests
+
 def main():
-    pass
-
-
-
-
-
-
+    #https://api.vk.com/method/users.get?user_id=210700286&v=5.52
+    id = ''
+    access_token = 'bbe5afa6bbe5afa6bbe5afa685bb82c972bbbe5bbe5afa6e7ee4c05399f87e19973c1b7'
+    r = requests.get('https://api.vk.com/method/users.get?user_ids=210700286&v=5.74&access_token={}'.format(access_token))
+    print(r.json())
 
 if __name__ == '__main__':
     main()
