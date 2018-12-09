cb59f7bafbbbaa47f247efbaca7662896fdeaaa0
Leovair <Leovair@users.noreply.github.com>
2018-12-09 21:06:19 +0200

    Базовый файл в котором будет использоваться программа

--- mainVK.py
+++ mainVK.py
@@ -1,11 +1,17 @@
 import requests
+import json
+
+def write_json(data):
+    with open('person.json', 'w') as file:
+        json.dump(data, file, indent=2, ensure_ascii=False)
 
 def main():
-    #https://api.vk.com/method/users.get?user_id=210700286&v=5.52
-    id = ''
-    access_token = 'bbe5afa6bbe5afa6bbe5afa685bb82c972bbbe5bbe5afa6e7ee4c05399f87e19973c1b7'
-    r = requests.get('https://api.vk.com/method/users.get?user_ids=210700286&v=5.74&access_token={}'.format(access_token))
-    print(r.json())
+    # https://api.vk.com/method/users.get?user_id=210700286&v=5.52
+    id = '2974856'  # ID человека для запроса, пример: a_gamilkar или 2974856
+    access_token = ''  # Токен для запросов, получаем при создание Standalone приложение ВК.
+    r = requests.get(f'https://api.vk.com/method/users.get?user_ids={id}&v=5.74&access_token={access_token}')
+    write_json(r.json())
+
 
 if __name__ == '__main__':
     main()
