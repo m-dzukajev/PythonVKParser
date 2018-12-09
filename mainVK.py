250f0d2aee2b462a99aa8101e28679a61015aa53
Leovair <Leovair@users.noreply.github.com>
2018-12-09 21:07:38 +0200

    Добавление запроса по получению имени и фамилии

--- mainVK.py
+++ mainVK.py
@@ -8,7 +8,7 @@ def write_json(data):
 def main():
     # https://api.vk.com/method/users.get?user_id=210700286&v=5.52
     id = '2974856'  # ID человека для запроса, пример: a_gamilkar или 2974856
-    access_token = ''  # Токен для запросов, получаем при создание Standalone приложение ВК.
+    access_token = 'token'  # Токен для запросов, получаем при создание Standalone приложение ВК.
     r = requests.get(f'https://api.vk.com/method/users.get?user_ids={id}&v=5.74&access_token={access_token}')
     write_json(r.json())
 
